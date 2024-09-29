52e78534d33114b09469995a6bc33fa031086a0e
Zhen Huang <huangzhen7739@163.com>
2024-09-29 17:21:18 +0800

    modified:   README.md 	new file:   data/longCoT_from_prm800k/prm800k_cot_from_tree_train_678_v3.json 	new file:   data/sft_results/deepseek-math-7b-base-sft-math-shortcut.jsonl 	new file:   data/sft_results/deepseek-math-7b-base-sft-math-v3.jsonl 	new file:   data/sft_results/deepseek-math-7b-base-sft-math-vanilla.jsonl 	deleted:    network.html 	modified:   prm_800k.py 	modified:   utils.py

--- README.md
+++ README.md
@@ -7,11 +7,14 @@
 ### PRM800k-based
 
 #### Tree -> LongCoT
-prm800k_cot_from_tree_train_678: 原始的基于PRM800k的CoT，但是有大量重复和冗余
 
 实际用于训练的CoT以及SFT后得到的模型：
 
 | Training Data                                | Model                                     |
 |----------------------------------------------|-------------------------------------------|
+| ground truth solution                         | deepseek-math-7b-base-sft-math-vanilla   |
+| prm800k_cot_from_tree_train_678_shortcut      | deepseek-math-7b-base-sft-math-shortcut  |
+| prm800k_cot_from_tree_train_678（大量重复和冗余）|         |
 | prm800k_cot_from_tree_train_678_v1            | deepseek-math-7b-base-sft-math-v1         |
 | prm800k_cot_from_tree_train_678_v2            | deepseek-math-7b-base-sft-math-v2         |
+| prm800k_cot_from_tree_train_678_v3            | deepseek-math-7b-base-sft-math-v3         |
--- prm_800k.py
+++ prm_800k.py
@@ -5,6 +5,80 @@ import re
 from utils import *
 
 
+correctness_map = {
+    "✅": True,
+    "❌": False,
+    "None": "None"
+}
+
+def display_baseline(sft_dir):
+    def calculate_overall_accuracy(df):
+        overall_count = 0
+        correct_count = 0
+        for index, row in df.iterrows():
+            overall_count += 1
+            if row['result']:
+                correct_count += 1
+        return correct_count / overall_count
+
+    accuracy_list = []
+    for file in os.listdir(sft_dir):
+        if file.endswith('.jsonl'):
+            file_path = os.path.join(sft_dir, file)
+            result_df = load_data(file_path)
+            accuracy = calculate_overall_accuracy(result_df)
+            accuracy_list.append((os.path.splitext(file)[0], accuracy))
+
+    # 将结果转换为 DataFrame
+    accuracy_df = pd.DataFrame(accuracy_list, columns=['File Name', 'Accuracy'])
+
+    # 按准确率排序
+    accuracy_df = accuracy_df.sort_values(by='Accuracy', ascending=True)
+    accuracy_df = accuracy_df.reset_index(drop=True)
+    
+    st.dataframe(accuracy_df)
+        
+        
+def filter_correct_problems_1(df):
+    correctness = st.selectbox("Select correctness", ["None", "✅", "❌"])
+    
+    if correctness == "None":
+        return df
+    
+    matching_indices = []
+    for idx in df.index:
+        if df.at[idx, 'result'] == correctness_map[correctness]:
+            matching_indices.append(idx)
+    filtered_df = df.loc[matching_indices]
+    return filtered_df
+
+
+def filter_correct_problems_2(df1, df2):
+    left, right = st.columns(2)
+    with left:
+        correctness1 = st.selectbox("Select correctness for the first file", ["None", "✅", "❌"])
+    with right:
+        correctness2 = st.selectbox("Select correctness for the second file", ["None", "✅", "❌"])
+    
+    matching_indices = []
+
+    for idx in df1.index:
+        assert df1.at[idx, 'id'] == df2.at[idx, 'id']
+        
+        result1 = df1.at[idx, 'result']
+        result2 = df2.at[idx, 'result']
+        
+        if ((correctness1 == "None" or result1 == correctness_map[correctness1]) and
+            (correctness2 == "None" or result2 == correctness_map[correctness2])):
+            matching_indices.append(idx)
+    
+    filtered_df1 = df1.loc[matching_indices]
+    filtered_df2 = df2.loc[matching_indices]
+    
+    return filtered_df1, filtered_df2
+
+
+
 def visualize_prm_800k():
     # Load the data based on user choice
     
@@ -23,74 +97,58 @@ def visualize_prm_800k():
             
         trees = read_json('data/prm800k_tree_train_678.json')
         
-    elif file_type == "SFT Results":
-        with st.sidebar:
-            show_baseline = st.checkbox("Show Baseline")
-            if show_baseline:
-                
-                def calculate_overall_accuracy(df):
-                    overall_count = 0
-                    correct_count = 0
-                    for index, row in df.iterrows():
-                        overall_count += 1
-                        if row['result']:
-                            correct_count += 1
-                    return correct_count / overall_count
-                            
-                accuracy_list = []
-                for file in os.listdir('./data/sft_results/'):
-                    if file.endswith('.jsonl'):
-                        file_path = os.path.join('./data/sft_results/', file)
-                        result_df = load_data(file_path)
-                        accuracy = calculate_overall_accuracy(result_df)
-                        # 将文件名和准确率存入列表
-                        accuracy_list.append((os.path.splitext(file)[0], accuracy))
-
-                accuracy_list.sort(key=lambda x: x[1])
-
-                for file_name, accuracy in accuracy_list:
-                    st.write(f"{file_name}: {accuracy}")
-                
         
+    elif file_type == "SFT Results":
+        show_baseline = st.checkbox("Show Baseline")
+        if show_baseline:
+            display_baseline('./data/sft_results')
+                
         file_choice = st.multiselect("Choose 1 or 2 Files", [os.path.splitext(file)[0] for file in os.listdir('./data/sft_results/') if file.endswith('.jsonl')], max_selections=2)
+        
+        st.subheader("Filtering")
+        
         if len(file_choice) == 1:
             df = load_data(f'data/sft_results/{file_choice[0]}.jsonl')
+            df = filter_correct_problems_1(df)
         elif len(file_choice) == 2:
             df = load_data(f'data/sft_results/{file_choice[0]}.jsonl')
             df_compare = load_data(f'data/sft_results/{file_choice[1]}.jsonl')
+            df, df_compare = filter_correct_problems_2(df, df_compare)
         else:
             st.warning("Please select at least 1 file to continue.")
             st.stop()
             
-        
 
     if 'selected_example' not in st.session_state:
         st.session_state.selected_example = 1
 
-
     # Create a dictionary to hold examples by difficulty level
     difficulty_levels = {1: [], 2: [], 3: [], 4: [], 5: []}
     for index, row in df.iterrows():
-        difficulty_levels[row['level']].append(index + 1)  # Assuming 'difficulty_level' is a key in your JSON
+        difficulty_levels[row['level']].append(index + 1)
+        
+    st.subheader("Select Example")
+    difficulty, example = st.columns(2)
+    with difficulty:
+        selected_level = st.selectbox("Select Difficulty Level", [1, 2, 3, 4, 5])
+    with example:
+        example_options = difficulty_levels[selected_level]
+        selected_example = st.selectbox("Select Example", example_options)
 
-    # Sidebar for difficulty levels
-    selected_level = st.sidebar.selectbox("Select Difficulty Level", [1, 2, 3, 4, 5])
-
-    # Sidebar for example selection based on difficulty level
-    example_options = difficulty_levels[selected_level]
-
-    selected_example = st.sidebar.selectbox("Select Example", example_options)
 
     st.session_state.selected_example = selected_example
     
-    row = df.iloc[st.session_state.selected_example - 1]
     
-    if len(file_choice) == 2:
-        row_compare = df_compare.iloc[st.session_state.selected_example - 1]
+    if file_type == "Synthetic longCoT":
+        row = df[df['idx'] == st.session_state.selected_example - 1].iloc[0]
+        if len(file_choice) == 2:
+            row_compare = df_compare[df_compare['idx'] == st.session_state.selected_example - 1].iloc[0]
+    elif file_type == "SFT Results":
+        row = df[df['id'] == st.session_state.selected_example - 1].iloc[0]
+        if len(file_choice) == 2:
+            row_compare = df_compare[df_compare['id'] == st.session_state.selected_example - 1].iloc[0]
         
-
-    
-
+        
     idx_col, subject_col, difficulty_col = st.columns(3)
     
     with idx_col:
